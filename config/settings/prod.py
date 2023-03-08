dd1e3ec541001f4e59532f7c852b75167cc12a2d
Sangho Cho <cho.sangho@outlook.com>
2023-03-08 16:55:48 +0900

    migrate to postgre

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,16 +6,16 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
-# env = environ.Env()
-# environ.Env.read_env(BASE_DIR / '.env')
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
-# DATABASES ={
-#     'default' : {
-#         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-#         'NAME' : env('DB_NAME'),
-#         'USER' : env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': '5432',
-#     }
-# }
+DATABASES ={
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : env('DB_NAME'),
+        'USER' : env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
