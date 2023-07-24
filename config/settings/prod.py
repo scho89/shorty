fc41dd28a95fee8a4c36937c36a859bb89651cce
ho <ho@scho.kr>
2023-07-25 08:31:41 +0900

    ad sense added and sql migration

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,13 +9,25 @@ DEBUG = False
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
+#DATABASES ={
+#    'default' : {
+#        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+#        'NAME' : env('DB_NAME'),
+#        'USER' : env('DB_USER'),
+#        'PASSWORD': env('DB_PASSWORD'),
+#        'HOST': env('DB_HOST'),
+#        'PORT': '5432',
+#    }
+#}
 DATABASES ={
     'default' : {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'ENGINE' : 'mssql',
         'NAME' : env('DB_NAME'),
         'USER' : env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': '5432',
+        'PORT': env('DB_PORT'),
+		'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server'},
     }
 }
+
