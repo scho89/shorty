68d828a2a09306c9598d3fd9bc4bf288d74abece
Sangho Cho <cho.sangho@outlook.com>
2023-07-26 10:25:18 +0900

    allowed hosts from db data

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,8 @@
-import environ
 from .base import *
+import environ
+import pymssql
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
@@ -9,6 +10,18 @@ DEBUG = False
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
+conn = pymssql.connect(host=env('DB_HOST'), database=env('DB_NAME'), charset='utf8',user=env('DB_USER'),password=env('DB_PASSWORD'))
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM shorty_domain WHERE host_allowed='TRUE';")
+
+row = cursor.fetchone()
+while row:
+    ALLOWED_HOSTS.append(row[1])
+    row = cursor.fetchone()
+
+conn.close()
+
+#
 #DATABASES ={
 #    'default' : {
 #        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
@@ -19,6 +32,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 #        'PORT': '5432',
 #    }
 #}
+
+
 DATABASES ={
     'default' : {
         'ENGINE' : 'mssql',
