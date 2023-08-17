8747c8b4acb1fb7fec9de64baa0becbf0099c382
Sangho Cho <cho.sangho@outlook.com>
2023-08-17 09:56:52 +0900

    update href tag

--- common/views.py
+++ common/views.py
@@ -227,6 +227,7 @@ def get_url_wc_data(surls):
         data['weight'] = round(surl.visit_counts/max(counts)*16)+1
         data['color'] = "#"+hex(randint(50,255))[2:]+hex(randint(50,255))[2:]+hex(randint(50,255))[2:]
         data['short_url'] = surl.short_url
+        data['url']=surl.url
         wc_data.append(data)
         
     for i in range(1,18):
